1cf50786fdcbf0e43631ec534fea721ac4dd1189
Daniel <003695626@coyote.csusb.edu>
2016-12-03 10:14:40 -0800

    Uploading Alderon defense game with working turret and falling objects

---
+++ Save Alderon/Assets/bombHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bombHealth : MonoBehaviour {
+
+	public int startingHealth = 2;
+	public int bomb;
+
+	// Use this for initialization	
+	void Start () {
+		bomb = startingHealth;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
