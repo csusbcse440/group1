2db6ab9196e0b6149b5759feded516000ca3adc3
Daniel <003695626@coyote.csusb.edu>
2016-12-06 12:30:10 -0800

    Targeting working again on Alderon

--- GodWhat/Assets/SaveAlderon/rayCastForward.cs
+++ GodWhat/Assets/SaveAlderon/rayCastForward.cs
@@ -16,30 +16,37 @@ public class rayCastForward : MonoBehaviour {
 		Raycasting ();
 
 		Vector2 cannonEnd = GameObject.Find ("cannonEnd").transform.position;
+		//cannonEnd.z = 0;
 		Vector2 formRightTriangle = cannonEnd;
+		//Debug.Log (cannonEnd);
 		formRightTriangle.y = GameObject.Find ("pivotPoint").transform.position.y;
 		Vector2 pivotPoint = GameObject.Find ("pivotPoint").transform.position;
 
 		Debug.DrawLine (cannonStart.position, formRightTriangle, Color.red); //adjacent
 		Debug.DrawLine (cannonEnd, formRightTriangle, Color.blue); //opposite
+		Debug.DrawLine (cannonEnd, cannonStart.position, Color.yellow); //opposite
 
-		float hypotenuseDistance = Vector2.Distance (cannonStart.position, cannonEnd);
+		float hypotenuseDistance = Vector2.Distance (cannonEnd, cannonStart.position);
+		//Debug.Log (hypotenuseDistance);
 
 		float oppositeDistance = Vector2.Distance (cannonEnd, formRightTriangle);
+		//Debug.Log (oppositeDistance);
 
 		float sin = Mathf.Asin (oppositeDistance / hypotenuseDistance);
 		sin = sin * 180 / Mathf.PI;
 
-		if (cannonEnd.y < -5) {
+		//if (cannonEnd.y < -5) {
 			//Debug.Log ("false");
-		} else if (cannonEnd.y > -5) {
+		//} else if (cannonEnd.y > -5) {
 			//Debug.Log ("true");
-		}
+		//}
 
-		if (cannonEnd.x < 0 && cannonEnd.y > -5) {
+		if (cannonEnd.x < 17 && cannonEnd.y > -7.6) {
 			GameObject.Find ("pivotPoint").transform.eulerAngles = new Vector3 (0, 0, (90 - sin));
-		} else if (cannonEnd.x >= 0 && cannonEnd.y > -5) {
+			//Debug.Log ("sin " + sin);
+		} else if (cannonEnd.x >= 17 && cannonEnd.y > -7.6) {
 			GameObject.Find ("pivotPoint").transform.eulerAngles = new Vector3 (0, 0, (sin - 90));
+			//Debug.Log ("sin " + sin);
 		} 
 
 		if (Input.GetMouseButtonDown (0)) {
--- GodWhat/Assets/SaveAlderon/updateCannonEndPoint.cs
+++ GodWhat/Assets/SaveAlderon/updateCannonEndPoint.cs
@@ -13,6 +13,8 @@ public class updateCannonEndPoint : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 mark = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mark.y = mark.z;
+		mark.z = -26;
 		//Debug.Log (mark);
 		transform.position = mark;
 	}
